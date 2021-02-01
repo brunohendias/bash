8cc73a0eeb0dccb05f03355f902c0c6e196aab53
brunodiashen <brunodiashen@gmail.com>
2021-02-01 20:51:03 -0300

    configurar arquivos bash, comandos para manipular grupo e usuario

---
+++ configurabash.sh
@@ -0,0 +1,3 @@
+cd arquivos_bash
+cp .bash* $HOME
+echo '[ARQUIVOS .bash COPIADOS PARA A $HOME COM SUCESSO]'
---
+++ usuarioslinux.sh
@@ -0,0 +1,15 @@
+sudo adduser teste # Cria o usuario
+
+sudo adduser teste sudo # Adiciona o usuario no grupo de sudo
+
+sudo deluser teste # Remove o usuario do grupo do sudo
+
+cat /etc/passwd # Visualizar os usuarios existentes
+
+sudo deluser teste # Deleta o usuario
+
+sudo addgroup literal # Cria um grupo
+
+cat /etc/group # Visualizar os grupos existentes
+
+sudo delgroup literal # Deleta o grupo
