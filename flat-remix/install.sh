a16d5b7c10d8027dea2d3417ebb0519ff8bf68c5
Bruno <brunodiashen@gmail.com>
2021-02-27 12:48:17 -0300

    copiar arquivos

--- flat-remix/install.sh
+++ flat-remix/install.sh
@@ -7,8 +7,8 @@ cd ../.themes
 tar -xvf 06-Flat-Remix-GTK-Blue-Dark-Solid_20201129.tar.xz Flat-Remix-GTK-Blue-Dark-Solid
 cd ../
 echo -e "\n[MOVENDO PASTAS PARA O DIRETORIO HOME]"
-cp .icons $HOME;
-cp .themes $HOME;
+cp -r .icons $HOME;
+cp -r .themes $HOME;
 echo -e "\n[TROCANDO O THEME E O ICON]"
 gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Blue-Dark-Solid"
 gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix-Blue-Dark"
