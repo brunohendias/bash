bb50fd1a81d5e7ff82fab93f3048f4d57426c878
brunohendias <brunodiashen@gmail.com>
2021-05-22 13:22:27 -0300

    remover arquivos descompactados

--- flat-remix/install.sh
+++ flat-remix/install.sh
@@ -11,3 +11,6 @@ echo -e "\n[TROCANDO O THEME E O ICON]"
 gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Blue-Dark-Solid"
 gsettings set org.gnome.desktop.interface icon-theme "Flat-Remix-Blue-Dark"
 echo -e "\n[FINALIZADO E CONFIGURADO]"
+echo -e "\n[DELETANDO PASTAS DESCOMPACTADAS]"
+rm -rf .icons/Flat-Remix-Blue-Dark/ 
+rm -rf .themes/Flat-Remix-GTK-Blue-Dark-Solid/
